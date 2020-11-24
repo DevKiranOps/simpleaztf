74c2e6d61ee9bdc76670e5e7e8e37b782dc0c151
KK <kiranopskumar@gmail.com>
2020-11-24 08:38:37 +0530

    Webserver-0 added

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ provider "azurerm" {
   features {}
 }
 
-
 # Create a resource group
 resource "azurerm_resource_group" "myapp" {
   name     = "tf-demo"
@@ -38,7 +37,72 @@ resource "azurerm_subnet" "myapp" {
   
 }
 
+resource "azurerm_subnet" "myapp-backend" {
+  name                 = "backend"
+  resource_group_name  = azurerm_resource_group.myapp.name
+  virtual_network_name = azurerm_virtual_network.myapp.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  
+}
 
 
 
+
+resource "azurerm_virtual_machine" "myapp" {
+  name                  = "webserver-0"
+  location              = azurerm_resource_group.myapp.location
+  resource_group_name   = azurerm_resource_group.myapp.name
+  network_interface_ids = [azurerm_network_interface.myapp.id]
+  vm_size               = "Standard_B1ls"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "webserver-0"
+    admin_username = "azadmin"
+    
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      key_data = file("~/.ssh/id_rsa.pub")
+      path     = "/home/azadmin/.ssh/authorized_keys"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
+
+
+resource "azurerm_network_interface" "myapp" {
+  name                = "webserver-0"
+  location            = azurerm_resource_group.myapp.location
+  resource_group_name = azurerm_resource_group.myapp.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.myapp.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
